f6c310ad8acbdd05161568a82f9da7b4c5fb9539
Saurabh Dixit <smartds2550@gmail.com>
2023-07-14 12:50:45 +0000

    Updated nodemailer.js

--- configration/nodemailer.js
+++ configration/nodemailer.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 // Transporter
 module.exports.transporter = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
-  port: 587,
+  port: process.env.NODEMAILER_PORT,
   auth: {
     user: process.env.NODEMAILER_USERNAME,
     pass: process.env.NODEMAILER_PASSWORD,
