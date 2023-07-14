2daa0afef725a55a8734e11ba4fd219e57221ccb
Saurabh Dixit <smartds2550@gmail.com>
2023-07-14 15:16:58 +0000

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use('*',(req,res) => {
 });
 app.use((err, req, res, next) => {
   logger.error(err.message);
-  next(err);
+  next();
 });
 // Function to check the database connection status
 const checkDatabaseConnection = () => {
