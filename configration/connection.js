683326f7502e6655cec9280e6906e79c94e25445
Saurabh Dixit <smartds2550@gmail.com>
2023-07-14 16:36:35 +0000

    Update connection.js

--- configration/connection.js
+++ configration/connection.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
-
+// console.log(`MongoDB Database connected in Successfully : ${con.connection.host}`);
 const connectDB = async () => {
   try {
     // mongodb connection
-    const con = mongoose.connect(process.env.MONGO_URL, {
+    const con = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    const isConnected = mongoose.connection.readyState === 1; // 1 = connected
-    if (isConnected) {
-      console.log(
-        `MongoDB Database connected in Successfully and Database connection is active in : ${con.connection.host}`
-      );
-    } else {
+    const isConntected = await mongoose.connection.readyState === 1; // 1 = connected
+    if(isConntected){
+       console.log(`MongoDB Database connected in Successfully and Database connection is active in :  ${con.connection.host}`);
+    }else{
       console.log('Database connection is not active');
     }
+      
   } catch (err) {
     process.exit(1);
   }
